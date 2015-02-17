db5155c0b4c9430eac15f4b2f576ab7d621510d9
Andrew <andrew@syn0.com>
2015-02-17 00:32:31 -0700

    Using object.bind

--- server/db.js
+++ server/db.js
@@ -22,8 +22,8 @@ projectDb  = Redis.createClient({
 db.use(bodyParser.json());
 
 function create(database, req, res) {
-    database.exists(req.body.name).then(function (reply) {
-        if (! reply) {
+    database.exists(req.body.name).then(function (exists) {
+        if (! exists) {
             database.hmset(req.body.name, req.body);
         } else {
             res.sendStatus(422);
@@ -34,24 +34,36 @@ function create(database, req, res) {
 }
 
 function read(database, req, res) {
-    database.hget(req.params.key).then(function (reply) {
-        res.send(reply);
-        res.end();
+    database.exists(req.params.key).then(function (exists) {
+        if (exists) {
+            database.hget(req.params.key).then(function (item) {
+                res.send(item);
+                res.end();
+            });
+        } else {
+            res.sendStatus(404);
+            res.end();
+        }
     });
 }
 
 function readAll(database, req, res) {
-    database.keys('*').then(function (reply) {
-        database.hgetall(reply).then(function (reply) {
-            res.send(reply);
+    database.keys('*').then(function (keys) {
+        if (keys.length) {
+            database.hgetall(keys).then(function (items) {
+                res.send(items);
+                res.end();
+            });
+        } else {
+            res.send([]);
             res.end();
-        });
+        }
     });
 }
 
 function update(database, req, res) {
-    database.exists(req.params.key).then(function (reply) {
-        if (reply) {
+    database.exists(req.params.key).then(function (exists) {
+        if (exists) {
             database.hmset(req.params.key, req.body);
         } else {
             res.sendStatus(422);
@@ -62,8 +74,8 @@ function update(database, req, res) {
 }
 
 function destroy(database, req, res) {
-    database.exists(req.params.key).then(function (reply) {
-        if (reply) {
+    database.exists(req.params.key).then(function (exists) {
+        if (exists) {
             instanceDb.hdel(req.params.key);
         } else {
             res.sendStatus(422);
@@ -73,52 +85,17 @@ function destroy(database, req, res) {
     });
 }
 
-db.delete('/db/instance/:key', function (req, res) {
-    destroy(instanceDb, req, res);
-});
-
-db.get('/db/:key/instance', function (req, res) {
-    read(instanceDb, req, res);
-});
-
-db.get('/db/instances', function (req, res) {
-    readAll(instanceDb, req, res);
-});
-
-db.patch('/db/instance/:key', function (req, res) {
-    update(instanceDb, req, res);
-});
-
-db.post('/db/instance', function (req, res) {
-    create(instanceDb, req, res);
-});
-
-db.put('/db/instance/:key', function (req, res) {
-    update(instanceDb, req, res);
-});
-
-db.delete('/db/project/:key', function (req, res) {
-    destroy(projectDb, req, res);
-});
-
-db.get('/db/:key/project', function (req, res) {
-    read(projectDb, req, res);
-});
-
-db.get('/db/projects', function (req, res) {
-    readAll(projectDb, req, res);
-});
-
-db.patch('/db/project/:key', function (req, res) {
-    update(projectDb, req, res);
-});
-
-db.post('/db/project', function (req, res) {
-    create(projectDb, req, res);
-});
-
-db.put('/db/project/:key', function (req, res) {
-    update(projectDb, req, res);
-});
+db.delete('/db/instance/:key', destroy.bind(undefined, instanceDb));
+db.delete('/db/project/:key', destroy.bind(undefined, projectDb));
+db.get('/db/instance/:key', read.bind(undefined, instanceDb));
+db.get('/db/project/:key', read.bind(undefined, projectDb));
+db.get('/db/instances', readAll.bind(undefined, instanceDb));
+db.get('/db/projects', readAll.bind(undefined, projectDb));
+db.patch('/db/instance/:key', update.bind(undefined, instanceDb));
+db.patch('/db/project/:key', update.bind(undefined, projectDb));
+db.post('/db/instance', create.bind(undefined, instanceDb));
+db.post('/db/project', create.bind(undefined, projectDb));
+db.put('/db/instance/:key', update.bind(undefined, instanceDb));
+db.put('/db/project/:key', update.bind(undefined, projectDb));
 
 module.exports = db;
