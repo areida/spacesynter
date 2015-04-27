bf452dcd09a89965816ee7337e5c3b1be2433ad7
Andrew <andrew@syn0.com>
2015-04-27 02:38:49 -0700

    Cheffing away

--- cookbooks/spacesynter/recipes/webservers.rb
+++ cookbooks/spacesynter/recipes/webservers.rb
@@ -3,7 +3,10 @@ node[:deploy].each do |application, deploy|
         template "/etc/nginx/sites-available/#{application}" do
             mode 0644
             source 'nginx.site.erb'
-            variables node[:webserver].merge(node[:webservers][application])
+            variables ({
+                :deploy => deploy,
+                :webserver => node[:webserver].merge(node[:webservers][application])
+            })
             cookbook 'spacesynter'
         end
     end
